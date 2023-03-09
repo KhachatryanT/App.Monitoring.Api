3e8d8623f1aed862cdb1967e0d8b5815e4f57ed8
khachatriants <khachatriants@quantumart.ru>
2023-03-09 16:06:16 +0300

    #1 ass perion on the end

--- App.Monitoring.Api/Contracts/CreateDeviceStatisticRequest.cs
+++ App.Monitoring.Api/Contracts/CreateDeviceStatisticRequest.cs
@@ -5,7 +5,7 @@ namespace App.Monitoring.Api.Contracts;
 /// <summary>
 /// Модель создания новой статистики устройства.
 /// </summary>
-/// <param name="DeviceType">Тип устройства/</param>
+/// <param name="DeviceType">Тип устройства.</param>
 /// <param name="UserName">Имя пользователя.</param>
 /// <param name="ClientVersion">Версия клиента.</param>
 public sealed record CreateDeviceStatisticRequest(DeviceType DeviceType, string? UserName, string? ClientVersion);
