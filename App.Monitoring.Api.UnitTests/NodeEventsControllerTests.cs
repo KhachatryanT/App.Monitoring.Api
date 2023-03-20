1d2416f5e2171e77fb665cc9b676c2012d62676d
Tigran Khachatrian <khachatriants@quantumart.ru>
2023-03-20 09:30:44 +0300

    #4 after review: simplify unit tests

--- App.Monitoring.Api.UnitTests/NodeEventsControllerTests.cs
+++ App.Monitoring.Api.UnitTests/NodeEventsControllerTests.cs
@@ -1,12 +1,8 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using App.Monitoring.Api.Contracts;
-using App.Monitoring.Entities.Entities;
-using App.Monitoring.Entities.Enums;
-using App.Monitoring.Infrastructure.Interfaces.DataAccess;
 using App.Monitoring.UseCases.Handlers.NodesEvents.Commands.CreateNodeEvents;
 using App.Monitoring.UseCases.Handlers.NodesEvents.Queries.GetNodeEvents;
 using AutoFixture.Xunit2;
@@ -23,10 +19,8 @@ public class NodeEventsControllerTests
 {
     /// <summary>
     /// Добавить события узла.
-    /// Должен быть вызван метод репозитория для добавления записей.
+    /// Должен быть вызван MediatR с правильным типом.
     /// </summary>
-    /// <param name="nodesRepositoryMoq">Moq репозитория узлов.</param>
-    /// <param name="nodesEventsRepositoryMoq">Moq репозитория событий узлов.</param>
     /// <param name="senderMoq">Moq MediatR.</param>
     /// <param name="controller">Контроллер.</param>
     /// <param name="nodeIdRequest">Идентификатор узла.</param>
@@ -34,64 +28,38 @@ public class NodeEventsControllerTests
     /// <returns>Task.</returns>
     [Theory]
     [AutoMoqData]
-    public async Task CreateNodeEvents_ShouldCreateRepositoryMethodCalled(
-        [Frozen] Mock<INodesRepository> nodesRepositoryMoq,
-        [Frozen] Mock<INodeEventsRepository> nodesEventsRepositoryMoq,
+    public async Task CreateNodeEvents_SenderMethodWasCalledWithCorrectTypeExpected(
         [Frozen] Mock<ISender> senderMoq,
         NodeEventsController controller,
         Guid nodeIdRequest,
         IEnumerable<NodeEvent> nodeEventsRequest)
     {
-        // Arrange
-        nodesRepositoryMoq.Setup(x => x.GetAsync(It.IsAny<Guid>(), It.IsAny<CancellationToken>()))
-            .Returns(() => Task.FromResult((NodeEntity?)new NodeEntity(nodeIdRequest, DeviceType.Android, "Ваня", "1.0.0", DateTimeOffset.UtcNow)));
-
-        nodesEventsRepositoryMoq.Setup(repo => repo.CreateAsync(It.IsAny<IEnumerable<NodeEventEntity>>(), It.IsAny<CancellationToken>()));
-
-        var handler = new CreateNodeEventsCommandHandler(nodesRepositoryMoq.Object, nodesEventsRepositoryMoq.Object);
-
-        senderMoq.Setup(x => x.Send(It.IsAny<CreateNodeEventsCommand>(), It.IsAny<CancellationToken>()))
-            .Returns<CreateNodeEventsCommand, CancellationToken>((request, cToken) => handler.Handle(request, cToken));
-
         // Act
-        _ = await controller.CreateNodeEvents(nodeIdRequest, nodeEventsRequest);
+        await controller.CreateNodeEvents(nodeIdRequest, nodeEventsRequest);
 
         // Assert
-        nodesEventsRepositoryMoq.Verify(x => x.CreateAsync(It.IsAny<IEnumerable<NodeEventEntity>>(), It.IsAny<CancellationToken>()), Times.Once);
+        senderMoq.Verify(x => x.Send(It.IsAny<CreateNodeEventsCommand>(), It.IsAny<CancellationToken>()), Times.Once);
     }
 
     /// <summary>
     /// Получение событий узла.
-    /// Должны вернуться все события узла.
+    /// Должен быть вызван MediatR с правильным типом.
     /// </summary>
-    /// <param name="nodesEventsRepositoryMoq">Moq репозитория событий узлов.</param>
     /// <param name="senderMoq">Moq MediartR.</param>
     /// <param name="controller">Контроллер.</param>
     /// <param name="nodeIdRequest">Идентификатор узла.</param>
-    /// <param name="expectedNodeEvents">События.</param>
     /// <returns>Task.</returns>
     [Theory]
     [AutoMoqData]
-    public async Task GetNodeEvents_ShouldReturnEvents(
-        [Frozen] Mock<INodeEventsRepository> nodesEventsRepositoryMoq,
+    public async Task GetNodeEvents_SenderMethodWasCalledWithCorrectTypeExpected(
         [Frozen] Mock<ISender> senderMoq,
         NodeEventsController controller,
-        Guid nodeIdRequest,
-        IEnumerable<NodeEventEntity> expectedNodeEvents)
+        Guid nodeIdRequest)
     {
-        // Arrange
-        nodesEventsRepositoryMoq.Setup(repo => repo.GetByNodeIdAsync(It.IsAny<Guid>(), It.IsAny<CancellationToken>()))
-            .Returns(() => Task.FromResult(expectedNodeEvents));
-
-        var handler = new GetNodeEventsQueryHandler(nodesEventsRepositoryMoq.Object);
-
-        senderMoq.Setup(x => x.Send(It.IsAny<GetNodeEventsQuery>(), It.IsAny<CancellationToken>()))
-            .Returns<GetNodeEventsQuery, CancellationToken>((request, cToken) => handler.Handle(request, cToken));
-
         // Act
-        var actualNodeEvents = await controller.GetNodeEvents(nodeIdRequest);
+        await controller.GetNodeEvents(nodeIdRequest);
 
         // Assert
-        Assert.Equal(expectedNodeEvents.Count(), actualNodeEvents.Events.Count());
+        senderMoq.Verify(x => x.Send(It.IsAny<GetNodeEventsQuery>(), It.IsAny<CancellationToken>()), Times.Once);
     }
 }
--- App.Monitoring.Api.UnitTests/NodesControllerTests.cs
+++ App.Monitoring.Api.UnitTests/NodesControllerTests.cs
@@ -1,9 +1,5 @@
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using App.Monitoring.Entities.Entities;
-using App.Monitoring.Infrastructure.Interfaces.DataAccess;
 using App.Monitoring.UseCases.Handlers.Nodes.Queries.GetNodes;
 using AutoFixture.Xunit2;
 using MediatR;
@@ -19,34 +15,21 @@ public class NodesControllerTests
 {
     /// <summary>
     /// Получить узлы.
-    /// Должны вернуться все узлы.
+    /// Должен быть вызван MediatR с правильным типом.
     /// </summary>
-    /// <param name="nodesRepositoryMoq">Moq репозитория узлов.</param>
     /// <param name="senderMoq">Moq MediartR.</param>
     /// <param name="controller">Контроллер.</param>
-    /// <param name="expectedNodes">Узлы.</param>
     /// <returns>Task.</returns>
     [Theory]
     [AutoMoqData]
-    public async Task GetNodes_ShouldReturnNodes(
-        [Frozen] Mock<INodesRepository> nodesRepositoryMoq,
+    public async Task GetNodes_SenderMethodWasCalledWithCorrectTypeExpected(
         [Frozen] Mock<ISender> senderMoq,
-        NodesController controller,
-        IEnumerable<NodeEntity> expectedNodes)
+        NodesController controller)
     {
-        // Arrange
-        nodesRepositoryMoq.Setup(repo => repo.GetAsync(It.IsAny<CancellationToken>()))
-            .Returns(() => Task.FromResult(expectedNodes));
-
-        var handler = new GetNodesQueryHandler(nodesRepositoryMoq.Object);
-
-        senderMoq.Setup(x => x.Send(It.IsAny<GetNodesQuery>(), It.IsAny<CancellationToken>()))
-            .Returns<GetNodesQuery, CancellationToken>((request, cToken) => handler.Handle(request, cToken));
-
         // Act
-        var actualNodes = await controller.GetNodes();
+        await controller.GetNodes();
 
         // Assert
-        Assert.Equal(expectedNodes.Count(), actualNodes.Count());
+        senderMoq.Verify(x => x.Send(It.IsAny<GetNodesQuery>(), It.IsAny<CancellationToken>()), Times.Once);
     }
 }
