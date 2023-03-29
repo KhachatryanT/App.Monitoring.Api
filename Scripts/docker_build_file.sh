a88e824566d5fb09ab6f8530aef853fd78ed7d91
KhachatrianTS <KhachatrianTS@quantumart.ru>
2023-03-29 13:20:32 +0300

    #7 using docker save instead of --output because of missing manifest file

--- Scripts/docker_build_file.sh
+++ Scripts/docker_build_file.sh
@@ -1,1 +1,3 @@
-docker buildx build -f ../App.Monitoring.Host/Dockerfile --platform linux/amd64,linux/arm64/v8 --output="type=tar,dest=../Publish/monitoring-api.tar" ../
+docker buildx build --platform linux/amd64 -f ../App.Monitoring.Host/Dockerfile -t monitoring-api:latest --load ../
+docker save -o ../Publish/monitoring-api.tar monitoring-api:latest
+docker image rm monitoring-api:latest
