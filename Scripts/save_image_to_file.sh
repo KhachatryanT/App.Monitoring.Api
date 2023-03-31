f33d44b4327c2ee1c6423f8e60a632b90b84073c
Tigran Khachatrian <khachatriants@quantumart.ru>
2023-03-31 10:22:51 +0300

    #7 after review

--- Scripts/save_image_to_file.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-mkdir ../Publish
-docker save -o ../Publish/monitoring-api.tar monitoring-api:latest
--- scripts/deploy/docker-compose.development.sh
+++ scripts/deploy/docker-compose.development.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker load --input monitoring-api.tar
 docker load --input monitoring-webui.tar
-docker-compose -f development/docker-compose.yml --env-file development/.env up -d
+docker-compose -f development/docker-compose.yml up -d
